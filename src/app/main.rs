4c6558c9897633a2a4070ff14a0c6ba7e64b8851
vadwwxz <vadwwxz@gmail.com>
2023-06-06 21:58:09 +0300

    Updated architecture, Cargo.toml

---
+++ src/app/main.rs
@@ -0,0 +1,15 @@
+#[macro_use]
+extern crate rocket;
+
+
+// #[launch]
+// fn rocket() -> _ {
+//     //let database = MongoRepo::init();
+//     rocket::build()
+//         // .manage(database)
+//         // .mount("/", routes![create_note])
+//         // .mount("/", routes![get_note])
+// }
+fn main() {
+    println!("Hello");
+}
